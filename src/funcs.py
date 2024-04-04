7fb74a01c74af06eafef5ebae654d25b3ca373d8
Vladimir Pristansky <pristanskyvs@mail.ru>
2024-04-04 18:08:09 +0300

    func - методы для работы программы main - основная функция, которая взаимодействует с пользователем

---
+++ src/funcs.py
@@ -0,0 +1,37 @@
+from class_vacancies import Vacancy
+
+def create_list_obj(list_obj):
+    """
+    Создание списка экз класса Vacancy
+    :param list_obj: данные для создания экз класса
+    :return: list_vacancies: список экз класса
+    """
+    list_vacancies = []
+    for item in list_obj:
+        list_vacancies.append(
+            Vacancy(item.get('name'),
+                    item.get('area', {}).get('name'),
+                    item.get('salary'),
+                    item.get('snippet', {}).get('requirement'),
+                    item.get('apply_alternate_url'),
+                    ))
+    return list_vacancies
+
+def sort_salary_from(vacs_list, user_sorting_number):
+    """
+    Функция для списка вакансий по зарплате
+    :param vacs_list: список вакансий
+    :param user_option_sort: опция сортировки
+    :return: отсортированный список
+    """
+
+    if user_sorting_number == 1:
+        vacs_list.sort()
+        print('Выбрано: Сортировка по возрастанию зарплаты')
+    if user_sorting_number == 2:
+        vacs_list.sort(reverse=True)
+        print('Выбрано: Сортировка по убыванию зарплаты')
+    if user_sorting_number == 3:
+        print('Выбрано: Без сортировки списка вакансий')
+    return vacs_list
+
---
+++ src/main.py
@@ -0,0 +1,56 @@
+from class_hh import HeadHunter
+from class_json import JSONSaver
+from funcs import create_list_obj, sort_salary_from
+
+def user_interaction():
+    ''' Функция для взаимодействия с пользователем '''
+
+    print("Добро пожаловать! Данное приложение поможет вам найти интересующую вас вакансию.")
+    user_vacancy = input("Введите интересующую вас вакансию: ").lower().strip()
+    hh = HeadHunter()
+    vacancy_from_hh = hh.get_vacancies(user_vacancy)
+    saving_vac = JSONSaver("../data/searching_vacs.json")
+    saving_vac.write_data(vacancy_from_hh)
+    user_sorting_number = int(input('Наберите номер для сортировки списка вакансий:\n'
+                            '1 - сортировка по возрастанию зарплаты\n'
+                            '2 - сортировка по убыванию зарплаты\n'
+                            '3 - без сортировки\n'))
+
+    # конвертирование в список
+    vacs_list = create_list_obj(vacancy_from_hh)
+
+    # Сортировка вакансий
+    sorted_vacs = sort_salary_from(vacs_list, user_sorting_number)
+
+    # Вывод топ-вакансий по желанию пользователя
+    user_top_number = int(input('Выберите топ-список вакансий для выводы на экран, для этого'
+                                ' нажмите соответсвующую цифру:\n'
+                                '1 - топ-5 вакансий\n'
+                                '2 - топ-10 вакансий\n'
+                                '3 - топ-15 вакансий\n'
+                                '4 - показать все вакансии\n'))
+    if user_top_number == 1:
+        for vac in sorted_vacs[:5]:
+            print(vac)
+    elif user_top_number == 2:
+        for vac in sorted_vacs[:10]:
+            print(vac)
+    elif user_top_number == 3:
+        for vac in sorted_vacs[:15]:
+            print(vac)
+    elif user_top_number == 4:
+        for vac in sorted_vacs:
+            print(vac)
+
+    # Завершение программы или запуск сначала
+    choose_another_vac = input("Хотите выбрать другую вакансию?\n y/n : \n").lower().strip()
+    if choose_another_vac in ['y', 'yes', 'да', 'д']:
+
+        user_interaction()
+    elif choose_another_vac in ['n', 'no', 'нет', 'н']:
+        print('Программа завершена. Удачи!')
+        exit()
+
+
+if __name__ == "__main__":
+    user_interaction()
